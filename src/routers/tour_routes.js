bc79456032e76f8539ccef7eb174a1eb930c0a73
Ithan <ithan.camacho@epn.edu.ec>
2024-11-14 15:16:47 -0500

    actu

--- src/routers/tour_routes.js
+++ src/routers/tour_routes.js
@@ -2,16 +2,25 @@ import {Router} from 'express'
 import { createTourController, deleteTourController, getAllToursController, searchToursController, updateTourController } from '../controllers/tour_controller.js'
 const router = Router()
 
+//TOURS
+
+//Pública
 router.get('/tours',getAllToursController)
 
+//Privada - Admin o Empleado
 router.post('/tours',createTourController)
 
+//Privada - Admin o Empleado
 router.put('/tours/:id',updateTourController)
 
+//Privada - Admin o Empleado
 router.delete('/tours/:id',deleteTourController)
 
+//Pública
 router.get('/tours/:id',searchToursController)
 
+//---------------------------0----------------------------------0-------------------------------0--------------
 
+//Users
 
 export default router
